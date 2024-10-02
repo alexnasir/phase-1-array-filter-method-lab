c0c4a34a74ee982191f0bc174acab1e16ff16060
alex <alexnasiali45@gmail.com>
2024-10-02 18:38:09 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+function findMatching(drivers,name) {
+  return drivers.filter(driver => {
+   return driver.toLowerCase() === name.toLowerCase()
+  })
+}
+
+
+
+
+function fuzzyMatch(drivers,letter) {
+function checkdrivers(driver){
+   if(driver.slice(0,letter.length) == letter) {
+      return driver
+   }
+}
+return drivers.filter(checkdrivers)
+}
+function matchName (drivers,driver){
+   function checkdrivers(obj){
+      for(let name in obj){
+         if (obj[name] === driver) {
+ return obj           
+         }
+      }
+   }
+   return drivers.filter(checkdrivers)
+}
+
